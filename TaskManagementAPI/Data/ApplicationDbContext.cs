6620201efe0e3d6d221b373beb82222b10b1cb05
Cocoon <fasegnun@gmail.com>
2023-01-02 21:38:53 -0500

    Adding the default connection string to the appsettings.json and the dependency inyection in the program.cs file

--- TaskManagementAPI/Data/ApplicationDbContext.cs
+++ TaskManagementAPI/Data/ApplicationDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace TaskManagementAPI.Data
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagementAPI.Model;
+
+namespace TaskManagementAPI.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Model.Task> Tasks { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Session> Sessions { get; set; }
     }
 }
